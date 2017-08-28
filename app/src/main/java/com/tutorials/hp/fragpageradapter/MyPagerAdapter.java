aaff3d09b4fc774fcfe654c1d072197a9301dd7b
Oclemy <oclemmi@gmail.com>
2017-08-28 07:24:10 +0300

    Updated FragmentPagerAdapter

--- app/src/main/java/com/tutorials/hp/fragpageradapter/MyPagerAdapter.java
+++ app/src/main/java/com/tutorials/hp/fragpageradapter/MyPagerAdapter.java
@@ -7,10 +7,16 @@ import android.support.v4.app.FragmentPagerAdapter;
 import java.util.ArrayList;
 
 /**
- * Created by Hp on 3/22/2016.
+ - Our MyPagerAdapter class.
+ - Is our FragmentPagerAdapter class hence derives from android.support.v4.app.FragmentPagerAdapter.
+ - Methods: getItem(),getCount(),getPageTitle(),addPage().
+ - We define an arraylist to hold our pages/fragments here.
+ - We define an addPage() method that takes a fragment object and adds it to our pages arraylist.
+ - Our constructor takes a support FragmentManager object.
  */
 public class MyPagerAdapter extends FragmentPagerAdapter {
 
+    //FRAGMENTS COLLECTION.
     ArrayList<Fragment> pages=new ArrayList<>();
 
     public MyPagerAdapter(FragmentManager fm) {
@@ -27,11 +33,13 @@ public class MyPagerAdapter extends FragmentPagerAdapter {
         return pages.size();
     }
 
+    //FRAGMENT TITLE
     @Override
     public CharSequence getPageTitle(int position) {
         return pages.get(position).toString();
     }
 
+    //ADD FRAGMENT TO COLLECTION.
     public void addPage(Fragment f)
     {
         pages.add(f);
